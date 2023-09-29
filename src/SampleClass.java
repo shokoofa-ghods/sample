4c9851743dfe7362338826f6280dce40ac967c4d
shokoofa-ghods <shokoofaghods@gmail.com>
2023-09-29 12:24:00 -0600

    a message was added locally.

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -1,5 +1,6 @@
 public class SampleClass {
     public static void main(String[] args) {
         System.out.println("Sample class was created");
+        System.out.println("a message created locally!");
     }
 }
