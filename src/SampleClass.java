317ca693380a83db1cee76d26bf8910eed2b368e
shokoofa-ghods <shokoofaghods@gmail.com>
2023-09-29 12:30:07 -0600

    a message was added in second branch.

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class SampleClass {
     public static void main(String[] args) {
         System.out.println("Sample class was created");
         System.out.println("message created remotely!");
+        System.out.println("a new message was added inside the second branch");
     }
 
 }
