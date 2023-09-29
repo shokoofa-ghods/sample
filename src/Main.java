a5cebecf0f1459d0ff96bc6a35b602b6f0c57d4e
shokoofa-ghods <56467180+shokoofa-ghods@users.noreply.github.com>
2023-09-29 11:28:21 -0600

    UniqueWordsCount method was added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+        public int uniqueWordsCount(String text) {
+        String[] words = text.split(" ");
+        HashSet<String> uniqueWords = new HashSet<>(Arrays.asList(words));
+        return uniqueWords.size();
+    }
 }
 
 class Avg {
