8f37b3c17a71ca7d23d0619a1e004f66e9b968e3
shokoofa-ghods <56467180+shokoofa-ghods@users.noreply.github.com>
2023-09-29 12:21:06 -0600

    a message was added remotely

--- src/SampleClass.java
+++ src/SampleClass.java
@@ -1,5 +1,7 @@
 public class SampleClass {
     public static void main(String[] args) {
         System.out.println("Sample class was created");
+        System.out.println("message created remotely!");
     }
+
 }
