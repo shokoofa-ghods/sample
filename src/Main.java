d69e9729556759f13a9ec588e57cba747ae13356
shokoofa-ghods <shokoofaghods@gmail.com>
2024-01-24 11:09:06 -0700

    Method two is added

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,9 @@ public class Main {
     public void method1(){
         System.out.println("this is method 1");
     }
+    public void method2(){
+        System.out.println("this is method 2");
+    }
 }
 
 class Avg {
